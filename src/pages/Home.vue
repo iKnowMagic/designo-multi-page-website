7f25b3b0825da419aa365493af026b8625d46627
iknowmagic <ilan@iknowmagic.net>
2020-10-25 09:25:17 -0700

    home / mobile

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -94,11 +94,11 @@
       </div>
       <div class="footer-menu">
         <div class="footer-menu-item">Our Company</div>
-        <div class="footer-menu-item">Location</div>
+        <div class="footer-menu-item">Locations</div>
         <div class="footer-menu-item">Contact</div>
       </div>
       <div class="footer-address">
-        <div class="line line-title">Designo Cental Office</div>
+        <div class="line line-title">Designo Central Office</div>
         <div class="line">3886 Wellington Street</div>
         <div class="line">Toronto, Ontario M9C 3J5</div>
       </div>
@@ -107,7 +107,38 @@
         <div class="line">P : +1 253-863-8967</div>
         <div class="line">M : contact@designo.co</div>
       </div>
-      <div class="footer-social"></div>
+      <div class="footer-social">
+        <a href="#">
+          <img
+            src="@/assets/images/shared/desktop/icon-facebook.svg"
+            alt="facebook"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="@/assets/images/shared/desktop/icon-youtube.svg"
+            alt="youtube"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="@/assets/images/shared/desktop/icon-twitter.svg"
+            alt="twitter"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="@/assets/images/shared/desktop/icon-pinterest.svg"
+            alt="pinterest"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="@/assets/images/shared/desktop/icon-instagram.svg"
+            alt="instagram"
+          />
+        </a>
+      </div>
     </div>
   </div>
 </template>
