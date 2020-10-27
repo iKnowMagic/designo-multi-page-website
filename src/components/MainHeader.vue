a85c3562cb5430669be85bbba31085cf1ed8b64a
iknowmagic <ilan@iknowmagic.net>
2020-10-27 14:24:04 -0700

    home / tablet progress

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -3,8 +3,13 @@
     <div class="logo">
       <img src="@/assets/images/shared/desktop/logo-dark.png" alt="logo" />
     </div>
-    <div class="menu">
+    <div class="menu-mobile">
       <img src="@/assets/images/shared/mobile/icon-hamburger.svg" alt="menu" />
+    </div>
+    <div class="menu">
+      <div class="menu-item">Our Company</div>
+      <div class="menu-item">Locations</div>
+      <div class="menu-item">Contact</div>
     </div>
   </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -72,61 +72,6 @@
         </div>
       </div>
     </div>
-    <div class="main-footer">
-      <div class="footer-logo">
-        <img
-          src="@/assets/images/shared/desktop/logo-light.png"
-          alt="designo"
-        />
-      </div>
-      <div class="footer-menu">
-        <div class="footer-menu-item">Our Company</div>
-        <div class="footer-menu-item">Locations</div>
-        <div class="footer-menu-item">Contact</div>
-      </div>
-      <div class="footer-address">
-        <div class="line line-title">Designo Central Office</div>
-        <div class="line">3886 Wellington Street</div>
-        <div class="line">Toronto, Ontario M9C 3J5</div>
-      </div>
-      <div class="footer-contact">
-        <div class="line line-title">Contact Us (Central Office)</div>
-        <div class="line">P : +1 253-863-8967</div>
-        <div class="line">M : contact@designo.co</div>
-      </div>
-      <div class="footer-social">
-        <a href="#">
-          <img
-            src="@/assets/images/shared/desktop/icon-facebook.svg"
-            alt="facebook"
-          />
-        </a>
-        <a href="#">
-          <img
-            src="@/assets/images/shared/desktop/icon-youtube.svg"
-            alt="youtube"
-          />
-        </a>
-        <a href="#">
-          <img
-            src="@/assets/images/shared/desktop/icon-twitter.svg"
-            alt="twitter"
-          />
-        </a>
-        <a href="#">
-          <img
-            src="@/assets/images/shared/desktop/icon-pinterest.svg"
-            alt="pinterest"
-          />
-        </a>
-        <a href="#">
-          <img
-            src="@/assets/images/shared/desktop/icon-instagram.svg"
-            alt="instagram"
-          />
-        </a>
-      </div>
-    </div>
   </div>
 </template>
 
